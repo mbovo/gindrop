d5cc4fd9bcf7371e8ac203a29970d7aaf06a84bd
Manuel Bovo <manuel.bovo@gmail.com>
2018-01-05 16:15:30 +0100

    Refactoring deploy() method

--- gindrop/swarm.py
+++ gindrop/swarm.py
@@ -209,29 +209,57 @@ class Manager(object):
         ydata = yaml.load(data)
 
         res = {}
-        i = 0
-        # if 'secrets' in ydata:
-        #     for secret in ydata['secrets']:
-        #         sec_obj = self.set_secret(secret, ydata['secrets'][secret])
-        #         res['sec_' + str(i)] = sec_obj.attrs
-        #         i += 1
-        # i = 0
-        # if 'configs' in ydata:
-        #     for config in ydata['configs']:
-        #         conf_obj = self.set_config(config, ydata['configs'][config])
-        #         res['conf_' + str(i)] = conf_obj.attrs
-        #         i += 1
-        # i = 0
-        if 'networks' in ydata:
-            for network in ydata['networks']:
-                net_obj = self.add_network(network, ydata['networks'][network])
-                res['net_' + str(i)] = net_obj.attrs
-                i += 1
 
-        i = 0
-        for service in ydata['services']:
-            srv_obj = self.add_service(service, ydata['services'][service])
-            res['srv_' + str(i)] = srv_obj.attrs
-            i += 1
+        funx_map = {
+            'secrets': self.set_config,
+            'configs': self.set_config,
+            'networks': self.add_network,
+            'services': self.add_service
+        }
+
+        for obj_name in funx_map.keys():
+            if obj_name in ydata:
+                for obj in ydata[obj_name]:
+                    i = 0
+                    try:
+                        if not ydata[obj_name][obj]['external']:
+                            ret_obj = funx_map[obj_name](obj, ydata[obj_name][obj])
+                            res[obj_name + str(i)] = ret_obj.attrs
+                            i += 1
+                        else:
+                            self.logger.info('{} [{}] is marked es external, skipping'.format(obj_name, obj))
+                    except Exception as e:
+                        self.logger.error('Cannot create {} [{}] due to: {}'.format(obj_name, obj, str(e)))
+        #
+        #
+        # # if 'secrets' in ydata:
+        # #     for secret in ydata['secrets']:
+        # #         sec_obj = self.set_secret(secret, ydata['secrets'][secret])
+        # #         res['sec_' + str(i)] = sec_obj.attrs
+        # #         i += 1
+        # # i = 0
+        # # if 'configs' in ydata:
+        # #     for config in ydata['configs']:
+        # #         conf_obj = self.set_config(config, ydata['configs'][config])
+        # #         res['conf_' + str(i)] = conf_obj.attrs
+        # #         i += 1
+        # # i = 0
+        # if 'networks' in ydata:
+        #     for network in ydata['networks']:
+        #         try:
+        #             if not ydata['networks'][network]['external']:
+        #                 net_obj = self.add_network(network, ydata['networks'][network])
+        #                 res['net_' + str(i)] = net_obj.attrs
+        #                 i += 1
+        #             else:
+        #                 self.logger.info('Network {} is marked as external, skpping'.format(network))
+        #         except Exception as e:
+        #             self.logger.error('Cannot create network {} for {}'.format(network, str(e)))
+        #
+        # i = 0
+        # for service in ydata['services']:
+        #     srv_obj = self.add_service(service, ydata['services'][service])
+        #     res['srv_' + str(i)] = srv_obj.attrs
+        #     i += 1
 
         return json.dumps(res)
